e41ee7c82bc827bcd5f630770e30defa0f879d1d
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-28 20:17:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+function bumpCounter() {
+	var counter = 0
+
+function addBump () {
+	counter += 1;
+	}
+
+function getBumps() {
+	return counter;
+}
+	return {
+		addBump,
+		getBumps
+	}
+}
+
+function createAnimal(animalType) {
+	return function (deadlyDevice) {
+
+		var object = {}
+
+		object.animalType = animalType
+		object.deadlyDevice = deadlyDevice
+		return object;
+	}
+}
+
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinCannon = sharkCreator('Cannon')
+
